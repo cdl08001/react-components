889090dc741e39a46707031ef946a6ca2924bc63
Calin Lewis <calin.lewis@gmail.com>
2018-06-27 12:24:06 -0700

    Finishes React Components

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+// // TODO
+// var Tomatoes = () => (
+//   <li>
+//       Tomatoes
+//   </li>
+// );
+
+// var FetaCheese = () => (
+//   <li>
+//       FetaCheese
+//   </li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+        textDecoration: this.state.done ? 'line-through' : 'none'
+    }
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.items}</li>
+    )
+  }
+}
+
+var GroceryList = (props) => (
+    <ul>
+      My Grocery List Items:
+      {console.log('Inside Grocery List: ', props)}
+      {props.items.map(item => 
+        <GroceryListItem items={item}/>
+      )}
+    </ul>
+);
+
+
+ReactDOM.render(<GroceryList items={['Pickles', 'Apples', 'Eggs']} />, document.getElementById("app"));
